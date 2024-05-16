e5a8b37898d36e0dfdccdee45277b9b72c41df8e
shanibjan <shanib.talrop@gmail.com>
2024-05-16 17:27:57 +0530

    final

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const sr=ScrollReveal({
+    origin:'bottom',
+    distance:'100px',
+    duration:'2300',
+})
+
+// sr.reveal('.div-2',{origin:'bottom',delay:'300',duration:'1500'})
+sr.reveal('.column',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.image-woman',{origin:'right',delay:'300',duration:'2000'})
+sr.reveal('.students-sub',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.views-3',{origin:'bottom',delay:'300',duration:'1500'})
+sr.reveal('.views-8',{origin:'bottom',delay:'500',duration:'1500'})
+sr.reveal('.views-13',{origin:'bottom',delay:'700',duration:'1500'})
+sr.reveal('.views-18',{origin:'bottom',delay:'900',duration:'1500'})
+sr.reveal('.deecee-5',{origin:'right',delay:'300',duration:'2000'})
+sr.reveal('.deecee-column',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.education',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.education-div-2',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.education-div',{origin:'right',delay:'300',duration:'2000'})
+sr.reveal('.app-column-2',{origin:'lef',delay:'300',duration:'2000'})
+sr.reveal('.app-column',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.message-column',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.message-column-2',{origin:'right',delay:'300',duration:'2000'})
+sr.reveal('.request-column',{origin:'left',delay:'300',duration:'2000'})
+sr.reveal('.request-column-2',{origin:'right',delay:'300',duration:'2000'})
+sr.reveal('.popular-slide-column',{origin:'bottom',delay:'300',duration:'2000'})
